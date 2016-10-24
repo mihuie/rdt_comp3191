90c6d9104f864f481af873d968913f535214d050
mihuie <huie.michael@gmail.com>
2016-10-23 22:44:17 -0500

    fixed bugs

--- Sender.py
+++ Sender.py
@@ -5,7 +5,6 @@ import Checksum
 import BasicSender
 from random import randint
 import time
-import re
 
 '''
 This is a skeleton sender class. Create a fantastic transport protocol here.
@@ -22,6 +21,7 @@ class Sender(BasicSender.BasicSender):
         self.msg_buffer = []
         self.seqno_buffer = []
         self.list_of_expected_seqno = []
+        self.validated_packts = []
 
     # Main sending loop.
     def start(self):
@@ -40,28 +40,19 @@ class Sender(BasicSender.BasicSender):
         stop = len(self.msg_buffer) + self.initial_seqnum + 1
         self.seqno_buffer = [x for x in range(start, stop)]
 
+        # reset variables 
+        self.validated_packts = []
+        self.recv_buffer = []   
         # sending packets
         while self.msg_buffer[self.running_index:] != []:
+
             self.list_of_expected_seqno = []
             # slicing already ack'd data off msg_buffer
             self.send_packets(self.msg_buffer[self.running_index:], self.seqno_buffer[self.running_index:])
             # check timeout
-            if self.recv_buffer == [] or self.recv_buffer is None:
-                pass
-            else:
-                if sackMode:
-                    # send last packet received
-                    self.validate_packet(self.recv_buffer[-1:][0], self.sackMode)
-                else:
-                    # validate recv packets
-                    for x in self.recv_buffer:
-                        self.validate_packet(x, self.sackMode)
-                
-                # update running_index to last successfully ack'd
-                try:
-                    self.running_index += self.list_of_expected_seqno.index(int(self.next_seqnum_to_receiver)) + 1
-                except:
-                    pass
+            self.recv_buffer = filter(None, self.recv_buffer)
+            if self.recv_buffer != [] and self.validate_packet(self.recv_buffer, self.sackMode):                
+                self.update_running_index()
 
     # Initate connection
     def initate_con(self):
@@ -72,63 +63,81 @@ class Sender(BasicSender.BasicSender):
             packet = self.make_packet('syn', self.initial_seqnum,'')
             self.send(packet,(dest, port))
             # set timer
-            recv = self.receive(0.5)
+            self.recv_buffer.append(self.receive(0.5))
             self.list_of_expected_seqno.append(self.initial_seqnum + 1)
+            self.recv_buffer = filter(None, self.recv_buffer)
             # validate data
-            if self.validate_packet(recv, self.sackMode):
+            if self.recv_buffer != [] and self.validate_packet(self.recv_buffer, self.sackMode):
                 return None
             # increment counter for attempts to reach receiver
             i+=1
         raise Exception ('Unable to reach Receiver')
 
     # Validate packet is not corrupt and update next_seqnum_to_receiver
-    def validate_packet(self, packet_recv, sackMode):
-        # checks connection time out
-        if packet_recv is None:
-            return False
+    def validate_packet(self, packets_recv, sackMode):
+        packets_recv = filter(None, packets_recv)
 
         if sackMode:
-            recv_msg_type, recv_seqno, recv_data, recv_checksum = self.split_packet(packet_recv)
-            if not(recv_msg_type == 'sack' and Checksum.validate_checksum(packet_recv) and recv_checksum != None):
-                return False
+            seqno = 0
+            buffd_at_receiver = []
 
-            # seperating proper ack from buffered at receiver 
-            recv_seqno = recv_seqno.split(';', 1)
-            recv_seqno = filter(None, recv_seqno)
-            seqno = int(recv_seqno[0])
+            self.list_of_expected_seqno = [int(x) for x in self.list_of_expected_seqno]
 
-            # check if packets buffered at receiver
-            if len(recv_seqno) == 1:
-                # checks if recv sequence number is expected
+            # import pdb; pdb.set_trace()
+            
+            # search for largest ack'd and most buffer at receiver to remedy packets arriving out of order
+            for x in packets_recv:
+                recv_msg_type, recv_seqno, recv_data, recv_checksum = self.split_packet(x)
+                if recv_msg_type == 'sack' and Checksum.validate_checksum(x) and recv_checksum != None:
+                    # seperating proper ack from buffered at receiver 
+                    recv_seqno = recv_seqno.split(';', 1)
+                    recv_seqno = filter(None, recv_seqno)
 
-                if int(seqno) not in self.list_of_expected_seqno:
-                    return False
-                # update next_seqnum_to_receiver with largest ack seqno
-                if seqno > self.next_seqnum_to_receiver and int(seqno) in self.list_of_expected_seqno:
-                    self.next_seqnum_to_receiver = int(seqno) 
-            else:
-                buffd_at_receiver = recv_seqno[1].split(',')
-                buffd_at_receiver = [int(x) + 1 for x in buffd_at_receiver]
-                
-                # quick resend
-                self.recv_buffer.append(self.receive(0.5))
+                if int(recv_seqno[0]) not in self.list_of_expected_seqno and int(recv_seqno[0]) != self.initial_seqnum + 1:
+                    continue
+
+                if int(recv_seqno[0]) > seqno:
+                    seqno = int(recv_seqno[0])
+                    try:
+                        buffd_at_receiver = [int(x) + 1 for x in recv_seqno[1].split(',')]
+                    except:
+                        buffd_at_receiver = []
+                elif int(recv_seqno[0]) == seqno:                      
+                    if len(recv_seqno) >  1:
+                        for x in [int(x) + 1 for x in recv_seqno[1].split(',')]:
+                            if x not in buffd_at_receiver:
+                                buffd_at_receiver.append(x)
+                else:
+                    seqno = self.initial_seqnum + 1
+
+            self.validated_packts.append(seqno)
+
+            if buffd_at_receiver != []:  
+                # quick resend                
                 for x in self.list_of_expected_seqno:
                     if x not in buffd_at_receiver:
                         num = self.running_index + self.list_of_expected_seqno.index(x) 
                         packet = self.make_packet('dat', self.seqno_buffer[num], self.msg_buffer[num])
                         self.send(packet,(dest, port)) 
-
-                # self.validate_packet(self.recv_buffer, sackMode)
-
+                        self.recv_buffer.append(self.receive(0.5))
         else:
             # check for ack and exsitence of a valid checksum
-            recv_msg_type, recv_seqno, recv_data, recv_checksum = self.split_packet(packet_recv)
-            if not(recv_msg_type == 'ack' and Checksum.validate_checksum(packet_recv) and recv_checksum != None):
-                return False
-            # checks if recv sequence number is expected
-            if int(recv_seqno) not in self.list_of_expected_seqno:
-                return False
-            self.update_running(recv_seqno)
+            for x in packets_recv:
+
+                recv_msg_type, recv_seqno, recv_data, recv_checksum = self.split_packet(x)
+                if not(recv_msg_type == 'ack' and Checksum.validate_checksum(x) and recv_checksum != None):
+                    return False
+                # checks if recv sequence number is expected
+                if int(recv_seqno) not in self.list_of_expected_seqno:
+                    return False
+
+                self.validated_packts.append(recv_seqno)
+
+                #fast retransmit
+                if packets_recv.count(x) >= 4:
+                    i = self.running_index + self.list_of_expected_seqno.index(recv_seqno)
+                    send_packets(self.msg_buffer[i], self.seqno_buffer[i])
+
         return True
 
     # Read and Break up file into messages
@@ -145,20 +154,32 @@ class Sender(BasicSender.BasicSender):
     # Send MAX packets (windows size)
     def send_packets(self, msgs, seqnos):
         i = 0
+
         self.recv_buffer = []
-        self.recv_buffer.append(self.receive(0.5))
-        while i < 7 and i < len(msgs):
-            packet = self.make_packet('dat', seqnos[i], msgs[i])
+        
+        while i < min(7, len(msgs)):
+            msg_type = 'dat'
+
+            # check if final message         
+            if self.seqno_buffer[-1:][0] == seqnos[i]:
+                msg_type = 'fin'
+
+            # send packets
+            packet = self.make_packet(msg_type, seqnos[i], msgs[i])
             self.send(packet,(dest, port))
             self.list_of_expected_seqno.append(seqnos[i] + 1)
+            self.recv_buffer.append(self.receive(0.5))
             i += 1
 
         self.recv_buffer = filter(None, self.recv_buffer)
 
-    # update next_seqnum_to_receiver with largest ack seqno
-    def update_running(self, seqno):
-        if seqno > self.next_seqnum_to_receiver and int(seqno) in self.list_of_expected_seqno:
-            self.next_seqnum_to_receiver = int(seqno)
+    # update index
+    def update_running_index(self):
+        # if not in list running index remains same
+        try:
+            self.running_index += self.list_of_expected_seqno.index(int(max(self.validated_packts))) + 1
+        except:
+            pass
 
 
 '''
